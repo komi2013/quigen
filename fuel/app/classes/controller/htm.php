2eaa2f9f66c244e8cd4391e31362812ee7efadfa
komikoma <komatsuka@yahoo.com>
2015-08-27 11:28:07 +0900

    404 error message

--- fuel/app/classes/controller/htm.php
+++ fuel/app/classes/controller/htm.php
@@ -8,6 +8,10 @@ class Controller_Htm extends Controller
       Model_Log::warn('no p');
       die( View::forge('404') );
     }
+    if ( !file_exists(APPPATH.'views/htm_'.$_GET['p'].'.php') ) {
+      Model_Log::warn('no php file');
+      die( View::forge('404') );
+    }
     $view = View::forge('htm_'.$_GET['p']);
     $view->u_id = Model_Cookie::get_usr();
     die($view);
@@ -16,13 +20,17 @@ class Controller_Htm extends Controller
   {
     Model_Csrf::setcsrf();
     $slash = explode( '/', $_SERVER['REQUEST_URI'] );
-    if ( isset($slash[2]) ) {
-      $view = View::forge('htm/'.$slash[2]);
-      $view->u_id = Model_Cookie::get_usr();
-      die($view);
-    } else {
+    if ( !isset($slash[2]) ) {
+      Model_Log::warn('no slash');
       die( View::forge('404') );
     }
+    if ( !file_exists(APPPATH.'views/htm/'.$slash[2].'.php') ) {
+      Model_Log::warn('no php file');
+      die( View::forge('404') );
+    }
+    $view = View::forge('htm/'.$slash[2]);
+    $view->u_id = Model_Cookie::get_usr();
+    die($view);
   }
 
 }
