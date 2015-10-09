803e1b36692def3ef75ef1d2fcd8cc44cbfa806c
komikoma <komatsuka@yahoo.com>
2015-10-09 13:56:39 +0900

    edit text

--- public/assets/js/quest.js
+++ public/assets/js/quest.js
@@ -124,8 +124,8 @@ if(ticket[0] < ticket[3]){
 }
 if(ticket[0] > 0){
   $('#ticket').css({ 'color': 'green' });
-  
   $('#ticket').empty().append(ticket[0]);
+  $('#light_2').empty().append('○下記の時間以降にチケットをもらえます');
   localStorage.ticket = JSON.stringify(ticket);
 }
 
