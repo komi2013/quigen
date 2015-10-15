bfda16bb98e377706102fc98ce9a3020162ce717
komikoma <komatsuka@yahoo.com>
2015-10-15 15:01:03 +0900

    treaty

--- fuel/app/classes/controller/4wordmoreadd.php
+++ fuel/app/classes/controller/4wordmoreadd.php
@@ -47,7 +47,7 @@ class Controller_4wordmoreadd extends Controller
           $answer_by_q->update_at = date("Y-m-d H:i:s");
           $answer_by_q->save();
           
-          DB::query("INSERT INTO tag (question_id,txt) VALUES (".$question_id.",'センター世界史')")->execute()->as_array();
+          DB::query("INSERT INTO tag (question_id,txt) VALUES (".$question_id.",'センター世界史')")->execute();
       
     }
     $res[0] = 1;
@@ -55,7 +55,7 @@ class Controller_4wordmoreadd extends Controller
   }
 
   if (isset($_GET['seq'])) {
-    $arr = DB::query("SELECT * FROM question WHERE id IN ( SELECT question_id FROM tag WHERE txt = 'センター世界史' ) ")->execute();
+    $arr = DB::query("SELECT * FROM question WHERE id IN ( SELECT question_id FROM tag WHERE txt = 'センター世界史' ) ORDER BY open_time ")->execute();
     $wh_time = 1438387200;
     foreach ($arr as $d) {
       $wh_time += 60;
@@ -66,27 +66,32 @@ class Controller_4wordmoreadd extends Controller
     die(json_encode($res));
   }  
 
-  $arr_word = DB::select()->from('word')
+  $arr_word = DB::select()->from('wh_treaty')
     ->execute()->as_array();
     //->order_by('quiz', 'asc')->limit(12)
 
     $ii = 1;
     //$arr_word_4 = array();
+    $wh_time = 1438387210;
+    
     foreach ($arr_word as $d) {
-      $arr_word_q[] = $d['quiz'];
-      $arr_word_a[] = $d['answer'];
+      //year,comment
+      $arr_word_q[] = $d['year'].'年、'.$d['comment'];
+      $arr_word_a[] = $d['name'];
 
       if ( ($ii % 4) == 0 ) {
         $i = 0;
+        
         while ($i < 4) {
+          $wh_time += 600;
           $question = new Model_Question();
           $question_id = $question->get_new_id();
           $question->id = $question_id;
-          $question->txt = $arr_word_q[$i].'の意味は？';
-          $question->usr_id = 22;
+          $question->txt = $arr_word_q[$i];
+          $question->usr_id = 33;
           $question->img = '';
           $question->create_at = date("Y-m-d H:i:s");
-          $question->open_time = date("Y-m-d H:i:s");
+          $question->open_time = date("Y-m-d H:i:s", $wh_time);
           $question->save();
           $arr_a = $arr_word_a;
           unset($arr_a[$i]);
@@ -107,27 +112,8 @@ class Controller_4wordmoreadd extends Controller
           $answer_by_q->update_at = date("Y-m-d H:i:s");
           $answer_by_q->save();
 
-          $a_news_time = new Model_ANewsTime();
-          $a_news_time->following_u_id = $usr_id;
-          $a_news_time->question_id = $question_id;
-          $a_news_time->q_img = '';
-          $a_news_time->u_img = ''; //change everytime image
-          $a_news_time->create_at = date( "Y-m-d H:i:s");
-          $a_news_time->save();
-
           $i++;
-          if ($arr_post[0]) {
-            $sql = "INSERT INTO tag (question_id,txt) VALUES (".$question_id.",'".$arr_post[0]."')";
-          }
-          if ($arr_post[1]) {
-            $sql = $sql.",(".$question_id.",'".$arr_post[1]."')";
-          }
-          if ($arr_post[2]) {
-            $sql = $sql.",(".$question_id.",'".$arr_post[2]."')";
-          }
-          if ($arr_post[0]) {
-            DB::query($sql)->execute();
-          }
+          DB::query("INSERT INTO tag (question_id,txt) VALUES (".$question_id.",'センター世界史')")->execute();
         }
         $arr_word_q = [];
         $arr_word_a = [];
