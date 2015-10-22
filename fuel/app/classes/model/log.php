24a51edf1d636fff19d1f62521d36d861c729dc7
komikoma <komatsuka@yahoo.com>
2015-10-22 11:15:08 +0900

    delete POST

--- fuel/app/classes/model/log.php
+++ fuel/app/classes/model/log.php
@@ -4,11 +4,6 @@ class Model_Log extends \Model
   public static function warn($warn) 
   {
     $warn = $_SERVER['REQUEST_URI'].' '.$warn;
-//    $warn = $warn.' GET ';
-//    foreach ($_GET as $d) {
-//      $warn = $warn.' '.$d;
-//    }
-    $warn = $warn.' POST ';
     foreach ($_POST as $d) {
       if ( is_array($d) ) {
         foreach ($d as $dd) {
