949a36fe8b41442f755c15753c4fda542d9210a5
komikoma <komatsuka@yahoo.com>
2015-11-27 15:51:55 +0900

    change monitor

--- fuel/app/views/monitor_mail.php
+++ fuel/app/views/monitor_mail.php
@@ -8,7 +8,7 @@ number of db                        ,<?=str_pad($arr_db[0],6)?>, <?=$arr_db[1]."
 used disk GB max 195GB              ,<?=str_pad($arr_disk[0],6)?>, <?=$arr_disk[1]." \r"?>
 vmstat (r) read                     ,<?=str_pad($arr_read[0],6)?>, <?=$arr_read[1]." \r"?>
 vmstat (w) write                    ,<?=str_pad($arr_write[0],6)?>, <?=$arr_write[1]." \r"?>
-vmstat (free+buff+cache) MB freemem ,<?=str_pad($arr_freemem[0],6)?>, <?=$arr_freemem[1]." \r"?>
+vmstat 2000MB - (free+buff+cache)   ,<?=str_pad($arr_freemem[0],6)?>, <?=$arr_freemem[1]." \r"?>
 vmstat(us + sy) cpu                 ,<?=str_pad($arr_cpu[0],6)?>, <?=$arr_cpu[1]." \r"?>
               -----------------------------
 <?=$err_log?>
