b5d130f288f596b47c11720efe2b54fd3309b0e7
komikoma <komatsuka@yahoo.com>
2015-11-27 15:17:20 +0900

    sitemap auto

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,7 +2,7 @@
 return array(
 	'_root_'  => 'top/index',  // The default route
 	'_404_'   => '404',    // The main 404 route
-       'htm/(:any)' => array('htm/all', 'name' => 'all'),
-
+        'htm/(:any)' => array('htm/all', 'name' => 'all'),
+        'sitemap2/(:any)' => array('sitemap2/all', 'name' => 'all'),
 // 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
