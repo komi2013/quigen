26cf7f70139d05cc0e8c63cb2c52d7ee97f681e3
komikoma <komatsuka@yahoo.com>
2015-11-27 16:52:45 +0900

    mistake routes.php

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -3,6 +3,6 @@ return array(
 	'_root_'  => 'top/index',  // The default route
 	'_404_'   => '404',    // The main 404 route
         'htm/(:any)' => array('htm/all', 'name' => 'all'),
-        'sitemap2/(:any)' => array('sitemap2/all', 'name' => 'all'),
+//        'sitemap2/(:any)' => array('sitemap2/all', 'name' => 'all'),
 // 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
