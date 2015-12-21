3fd277c846a85d09df6cacacab0582b051dcb5c0
komikoma <komatsuka@yahoo.com>
2015-12-21 12:39:57 +0900

    ver=46

--- fuel/app/views/request_point.php
+++ fuel/app/views/request_point.php
@@ -12,10 +12,10 @@
     <meta property="og:url" content="<?='http://'.Config::get("my.domain").'/pack/?p='.$_GET['p']?>" />
     <meta property="og:description" content="このクイズを答えたい場合、まずはポイントを購入してもらいそのポイントでクイズを購入してください" />
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
-    <script src="/assets/js/analytics.js?ver=46"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=46" />
-    <link rel="stylesheet" href="/assets/css/pc.css?ver=46" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=46" media="only screen and (max-width : 710px)">
+    <script src="/assets/js/analytics.js?ver=47"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=47" />
+    <link rel="stylesheet" href="/assets/css/pc.css?ver=47" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=47" media="only screen and (max-width : 710px)">
     <meta name="viewport" content="width=device-width, user-scalable=no" >
   </head>
 <body>
@@ -93,10 +93,10 @@
   <script>
     var csrf = '<?=Model_Csrf::setcsrf()?>';
   </script>
-<script src="/assets/js/basic.js?ver=46"></script>
-<script src="/assets/js/check_news.js?ver=46"></script>
+<script src="/assets/js/basic.js?ver=47"></script>
+<script src="/assets/js/check_news.js?ver=47"></script>
 <script src='https://www.paypalobjects.com/js/external/dg.js'></script>
-<script src="/assets/js/request_point.js?ver=46"></script>
+<script src="/assets/js/request_point.js?ver=47"></script>
 <script>
   ga('send','pageview','/request_point/');
 </script>
