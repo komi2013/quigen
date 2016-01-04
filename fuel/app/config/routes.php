8a82068b594e7c8a0bb2fef838ddbbaa5c0f39ce
komikoma <komatsuka@yahoo.com>
2016-01-04 17:31:25 +0900

    config change

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,8 +1,7 @@
 <?php
 return array(
-	'_root_'  => 'top/index',  // The default route
-	'_404_'   => '404',    // The main 404 route
+        '_root_'  => 'top/index',  // The default route
+        '_404_'   => '404',    // The main 404 route
         'htm/(:one)' => 'htm/all/$1',
         'sitemap/dynamic/(:tag)' => 'sitemap/dynamic/$1', 
-// 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
