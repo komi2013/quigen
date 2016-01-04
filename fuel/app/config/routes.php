c329590ed870b488cf681188b6f699e7fb668a36
komikoma <komatsuka@yahoo.com>
2016-01-04 17:35:20 +0900

    revert test routing

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -3,5 +3,6 @@ return array(
         '_root_'  => 'top/index',  // The default route
         '_404_'   => '404',    // The main 404 route
         'htm/(:one)' => 'htm/all/$1',
-        'sitemap/dynamic/(:tag)' => 'sitemap/dynamic/$1', 
+        'sitemap/dynamic/(:tag)' => 'sitemap/dynamic/$1',
+
 );
