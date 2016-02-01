a44dc5f66c44c42be7573f281601edfcd95aa699
komikoma <komatsuka@yahoo.com>
2016-02-01 10:12:15 +0900

    ver = ?

--- fuel/app/views/htm/news.php
+++ fuel/app/views/htm/news.php
@@ -8,10 +8,10 @@
     <script src="/third/jquery-2.1.1.min.js"></script>
     <script src="/third/jquery.cookie.js"></script>
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
-    <script src="/assets/js/analytics.js?ver=55"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=55" />
-    <link rel="stylesheet" href="/assets/css/pc.css?ver=55" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=55" media="only screen and (max-width : 710px)">
+    <script src="/assets/js/analytics.js?ver=56"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=56" />
+    <link rel="stylesheet" href="/assets/css/pc.css?ver=56" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=56" media="only screen and (max-width : 710px)">
     <meta name="viewport" content="width=device-width, user-scalable=no" >
   </head>
 <body>
@@ -36,8 +36,8 @@
 <script>
   var csrf = '<?=Model_Csrf::setcsrf()?>';
 </script>
-<script src="/assets/js/basic.js?ver=55"></script>
-<script src="/assets/js/news.js?ver=55"></script>
+<script src="/assets/js/basic.js?ver=56"></script>
+<script src="/assets/js/news.js?ver=56"></script>
 <script>
   ga('send', 'pageview');
 </script>
