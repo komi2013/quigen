c36379349a4b7c165c591b0d587a94f00b535c34
komatsuka <komatsuka@yahoo.com>
2016-03-23 14:56:25 +0900

    ver=64

--- fuel/app/views/generate_limited.php
+++ fuel/app/views/generate_limited.php
@@ -7,10 +7,10 @@
     <script src="/third/jquery-2.1.1.min.js"></script>
     <script src="/third/jquery.cookie.js"></script>
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
-    <script src="/assets/js/analytics.js?ver=63"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=63" />
-    <link rel="stylesheet" href="/assets/css/pc.css?ver=63" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=63" media="only screen and (max-width : 710px)">
+    <script src="/assets/js/analytics.js?ver=64"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=64" />
+    <link rel="stylesheet" href="/assets/css/pc.css?ver=64" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=64" media="only screen and (max-width : 710px)">
     <meta name="viewport" content="width=device-width, user-scalable=no" >
   </head>
 <body>
@@ -33,8 +33,8 @@
 </table>
 <?= View::forge('htm/ad_load') ?>
 </div>
-<script src="/assets/js/check_news.js?ver=63"></script>
-<script src="/assets/js/basic.js?ver=63"></script>
+<script src="/assets/js/check_news.js?ver=64"></script>
+<script src="/assets/js/basic.js?ver=64"></script>
 <script>
   ga('send', 'pageview', location.href+'&limited');
 </script>
