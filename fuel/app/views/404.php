119467f8e588a2a3ba86941c845fadc20f14ecfb
komatsuka <komatsuka@yahoo.com>
2016-04-21 14:50:20 +0900

    ver=66

--- fuel/app/views/404.php
+++ fuel/app/views/404.php
@@ -8,10 +8,10 @@
     <script src="/third/jquery-2.1.1.min.js"></script>
     <script src="/third/jquery.cookie.js"></script>
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
-    <script src="/assets/js/analytics.js?ver=65"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=65" />
-    <link rel="stylesheet" href="/assets/css/pc.css?ver=65" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=65" media="only screen and (max-width : 710px)">
+    <script src="/assets/js/analytics.js?ver=66"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=66" />
+    <link rel="stylesheet" href="/assets/css/pc.css?ver=66" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=66" media="only screen and (max-width : 710px)">
     <meta name="viewport" content="width=device-width, user-scalable=no" >
   </head>
 <body>
@@ -33,8 +33,8 @@
 
 <div id="ad_right"></div>
 
-<script src="/assets/js/check_news.js?ver=65"></script>
-<script src="/assets/js/basic.js?ver=65"></script>
+<script src="/assets/js/check_news.js?ver=66"></script>
+<script src="/assets/js/basic.js?ver=66"></script>
 <?php Model_Log::warn('Not Found');?>
 <script>
   $(function(){ ga('send', 'pageview', location.pathname + location.search + location.hash +':404'); });
