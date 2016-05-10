baf10722b3787c2ef33846d02bfa127b2315ea07
komatsuka <komatsuka@yahoo.com>
2016-05-10 17:54:20 +0900

    ver=69

--- fuel/app/views/404.php
+++ fuel/app/views/404.php
@@ -8,10 +8,10 @@
     <script src="/third/jquery-2.1.1.min.js"></script>
     <script src="/third/jquery.cookie.js"></script>
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
-    <script src="/assets/js/analytics.js?ver=68"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=68" />
-    <link rel="stylesheet" href="/assets/css/pc.css?ver=68" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=68" media="only screen and (max-width : 710px)">
+    <script src="/assets/js/analytics.js?ver=69"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=69" />
+    <link rel="stylesheet" href="/assets/css/pc.css?ver=69" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=69" media="only screen and (max-width : 710px)">
     <meta name="viewport" content="width=device-width, user-scalable=no" >
   </head>
 <body>
@@ -33,8 +33,8 @@
 
 <div id="ad_right"></div>
 
-<script src="/assets/js/check_news.js?ver=68"></script>
-<script src="/assets/js/basic.js?ver=68"></script>
+<script src="/assets/js/check_news.js?ver=69"></script>
+<script src="/assets/js/basic.js?ver=69"></script>
 <?php Model_Log::warn('Not Found');?>
 <script>
   $(function(){ ga('send', 'pageview', location.pathname + location.search + location.hash +':404'); });
--- fuel/app/views/quiz.php
+++ fuel/app/views/quiz.php
@@ -8,16 +8,16 @@
     <link rel="canonical" href="http://<?=Config::get('my.domain').'/quiz/?q='.$question?>" />
     <script src="/third/jquery-2.1.1.min.js"></script>
     <script src="/third/jquery.cookie.js"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/quiz.css?ver=68" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/quiz.css?ver=69" />
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
-    <script src="/assets/js/analytics.js?ver=68"></script>
+    <script src="/assets/js/analytics.js?ver=69"></script>
     <meta property="og:image" content="http://<?=$img ?: Config::get('my.domain').'/assets/img/icon/qg_big.png'?>" />
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=68" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css?ver=69" />
 <?php if( isset($_GET['iframe']) ){?>
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=68">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=69">
 <?php }else{ ?>
-    <link rel="stylesheet" href="/assets/css/pc.css?ver=68" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css?ver=68" media="only screen and (max-width : 710px)">
+    <link rel="stylesheet" href="/assets/css/pc.css?ver=69" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css?ver=69" media="only screen and (max-width : 710px)">
 <?php } ?>    
     <meta name="viewport" content="width=device-width, user-scalable=no" >
   </head>
@@ -191,9 +191,9 @@
   var comment_offline = '<?=urlencode($comment_offline)?>';
   comment_offline = decodeURIComponent(comment_offline).replace( /\+/g,' ');
 </script>
-<script src="/assets/js/basic.js?ver=68"></script>
-<script src="/assets/js/check_news.js?ver=68"></script>
-<script src="/assets/js/quiz.js?ver=68"></script>
+<script src="/assets/js/basic.js?ver=69"></script>
+<script src="/assets/js/check_news.js?ver=69"></script>
+<script src="/assets/js/quiz.js?ver=69"></script>
 <script>
 setTimeout(function(){
   ga('set', 'dimension7', iframe);
