99251602eb5308678f8c1340c56ab2a8e9db6118
komatsuka <komatsuka@yahoo.com>
2016-06-27 10:33:45 +0900

    remove open_time generate limit

--- fuel/app/classes/controller/generate.php
+++ fuel/app/classes/controller/generate.php
@@ -4,29 +4,30 @@ class Controller_Generate extends Controller
   public function action_index()
   {
     $usr_id = Model_Cookie::get_usr();
-    $query = DB::select()->from('question')
-      ->where('usr_id','=',$usr_id)
-      ->order_by('open_time', 'desc')
-      //->limit(20)
-      ->execute()->as_array();
-    
-    $now = new DateTime("now");
-    if ( isset($query[0]['open_time']) ) {
-      $available = new DateTime($query[0]['open_time']);
-      $now_limit = new DateTime("now");
-      $now_limit->add( new DateInterval('P30D') );
-      
-      if ( $query[0]['open_time'] > "2100-01-01 00:00:00" ) {
-        $available->sub( new DateInterval('P100Y') );
-      }
-      
-      if ($now_limit < $available) {
-        $available->sub( new DateInterval('P30D') );
-        $view = View::forge('generate_limited');
-        $view->available = $available->format('Y-m-d H:i:s');
-        
-        die($view);
-      }
+//    $query = DB::select()->from('question')
+//      ->where('usr_id','=',$usr_id)
+//      ->order_by('open_time', 'desc')
+//      //->limit(20)
+//      ->execute()->as_array();
+//    
+//    $now = new DateTime("now");
+//    if ( isset($query[0]['open_time']) ) {
+//      $available = new DateTime($query[0]['open_time']);
+//      $now_limit = new DateTime("now");
+//      $now_limit->add( new DateInterval('P30D') );
+//      
+//      if ( $query[0]['open_time'] > "2100-01-01 00:00:00" ) {
+//        $available->sub( new DateInterval('P100Y') );
+//      }
+//      
+//      if ($now_limit < $available) {
+//        $available->sub( new DateInterval('P30D') );
+//        $view = View::forge('generate_limited');
+//        $view->available = $available->format('Y-m-d H:i:s');
+//        
+//        die($view);
+//      }
+//      
 //      $last_open_time = new DateTime($query[0]['open_time']);
 //      $last_open_time->add( new DateInterval('PT1H'));
 //
@@ -38,7 +39,7 @@ class Controller_Generate extends Controller
 //      }
 //    } else {
 //      $open_time = $now->format('Y-m-d H:i:s');
-    }
+//    }
     //Model_Cookie::set('open_time', $open_time);
     $view = View::forge('generate');
     $view->u_id = $usr_id;
