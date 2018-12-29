6e54d78e994288299c94cc26e4f54302d11b8f97
[komatsu_s] <admin@example.com>
2018-12-29 11:17:07 +0900

    before release

--- fuel/app/config/prd_english/config.php
+++ fuel/app/config/prd_english/config.php
@@ -77,7 +77,31 @@ return array(
       
     'display_error' => false,
     'lang' => 'en',
-    'cache_v' => "?cache_v=98",
+    'cache_v' => "?cache_v=1"
   ),
-
+  'lang' => array(
+    'you_can_answer_after_this_time' => 'you can answer after this time',
+    'answer_first' => 'answer first',
+    'not_enough_point' => 'not enough point',
+    'thanks' => 'thanks. you will know <a href="/htm/?p=news">on this page</a>',
+    'del' => 'delete',
+    'confirm' => 'confirm',
+    'checked_chat' => 'checked chat',
+    'after' => 'after ',' hours_please' => ' hours please',
+    'made_quiz' => 'made quiz',
+    'no_' => 'No.', 'mon' => '',
+    'checked_offline' => 'checked offline',
+    'please_logout' => 'please logout delete',
+    'checked_mypage' => 'checked mypage',
+    'login' => 'login synchronize',
+    'change_profile' => 'change profile',
+    'logout' => 'logout',
+    'tag_category' => 'tag category',
+    'follow_confirm' => 'follow confirm',
+    'shared_quiz' => 'shared quiz',
+    'commented' => 'commented',
+    'report' => '#report ',
+    'checked_rank' => 'checked rank',
+    'checked_top' => 'checked top page',
+  ),
 );
