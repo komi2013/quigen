998aa9e7de31ed8e579f950ee2d3fe13bba76fab
seijiro.komatsu <seijiro.komatsu@shopairlines.com>
2020-01-06 17:04:44 +0900

    css in header

--- fuel/app/views/quiz.php
+++ fuel/app/views/quiz.php
@@ -8,6 +8,14 @@
     <link rel="canonical" href="https://<?=Config::get('my.domain').'/quiz/?q='.$question?>" />
     <meta property="og:image" content="https://<?=$img ?: Config::get('my.domain').'/assets/img/icon/qg_big.png'?>" />
     <meta name="viewport" content="width=device-width, user-scalable=no" >
+    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css<?=Config::get("my.cache_v")?>" />
+<?php if( isset($_GET['iframe']) ){?>
+    <link rel="stylesheet" href="/assets/css/sp.css<?=Config::get("my.cache_v")?>">
+<?php }else{ ?>
+    <link rel="stylesheet" href="/assets/css/pc.css<?=Config::get("my.cache_v")?>" media="only screen and (min-width : 711px)">
+    <link rel="stylesheet" href="/assets/css/sp.css<?=Config::get("my.cache_v")?>" media="only screen and (max-width : 710px)">
+<?php } ?>
+    <link rel="stylesheet" type="text/css" href="/assets/css/quiz.css<?=Config::get("my.cache_v")?>" />
   </head>
 <body>
 
@@ -161,16 +169,9 @@
 
     <script src="/third/jquery-2.1.1.min.js"></script>
     <script src="/third/jquery.cookie.js"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/quiz.css<?=Config::get("my.cache_v")?>" />
     <script>var ua = '<?=Config::get("my.ua")?>';</script>
     <script src="/assets/js/analytics.js<?=Config::get("my.cache_v")?>"></script>
-    <link rel="stylesheet" type="text/css" href="/assets/css/basic.css<?=Config::get("my.cache_v")?>" />
-<?php if( isset($_GET['iframe']) ){?>
-    <link rel="stylesheet" href="/assets/css/sp.css<?=Config::get("my.cache_v")?>">
-<?php }else{ ?>
-    <link rel="stylesheet" href="/assets/css/pc.css<?=Config::get("my.cache_v")?>" media="only screen and (min-width : 711px)">
-    <link rel="stylesheet" href="/assets/css/sp.css<?=Config::get("my.cache_v")?>" media="only screen and (max-width : 710px)">
-<?php } ?>    
+   
 <script>
   var q_id = '<?=$question?>';
   var usr = '<?=$usr?>';
